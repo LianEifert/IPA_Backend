e796f12dd522972db9248d982c3e6b1506713813
Eifert Lian <lian.eifert@ioz.ch>
2024-03-15 15:34:54 +0100

    Added Local CORS Policy

--- Program.cs
+++ Program.cs
@@ -7,31 +7,42 @@ using ProjectEstimaterBackend.Profiles;
 using ProjectEstimaterBackend.Services;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
-    {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-
-        var mapperConfig = new MapperConfiguration(cfg =>
+        .ConfigureFunctionsWebApplication(builder =>
         {
-            cfg.AddProfile<AddParticipantViewModelProfile>(); 
-            cfg.AddProfile<UpdateParticipantViewModelProfile>();
-            cfg.AddProfile<AddVotingViewModelProfile>();
-            cfg.AddProfile<UpdateVotingViewModelProfile>();
-            cfg.AddProfile<VotingViewModelProfile>();
-            cfg.AddProfile<ParticipantViewmodelProfile>();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("IocAppOrigins", policy =>
+                {
+                    policy.WithOrigins("http://localhost:3000")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+            });
+        })
+        .ConfigureServices((context, services) =>
+        {
+            services.AddApplicationInsightsTelemetryWorkerService();
+            services.ConfigureFunctionsApplicationInsights();
 
-        });
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<AddParticipantViewModelProfile>();
+                cfg.AddProfile<UpdateParticipantViewModelProfile>();
+                cfg.AddProfile<AddVotingViewModelProfile>();
+                cfg.AddProfile<UpdateVotingViewModelProfile>();
+                cfg.AddProfile<VotingViewModelProfile>();
+                cfg.AddProfile<ParticipantViewmodelProfile>();
 
-        IMapper mapper = mapperConfig.CreateMapper();
-        services.AddSingleton(mapper);
+            });
 
-        services.AddSingleton<IDataService<Participant>, ParticipantDataService>();
-        services.AddSingleton<IDataService<Voting>, VotingDataService>();
-        services.AddSingleton<IVotingDataService, VotingDataService>();
-        services.AddSingleton<IVotingParticipantService, VotingDataService>();
-    })
+            IMapper mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            services.AddSingleton<IDataService<Participant>, ParticipantDataService>();
+            services.AddSingleton<IDataService<Voting>, VotingDataService>();
+            services.AddSingleton<IVotingDataService, VotingDataService>();
+            services.AddSingleton<IVotingParticipantService, VotingDataService>();
+        })
     .Build();
 
 host.Run();
